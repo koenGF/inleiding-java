bccd86e0050e0927b6579160079529997d020f29
KoenGF <2144264@talnet.nl>
2020-09-14 21:24:09 +0200

    added Opdr1.java

---
+++ src/h06/Opdr1.java
@@ -0,0 +1,25 @@
+package h06;
+
+import java.awt.*;
+import java.applet.*;
+
+public class Opdr1 extends Applet {
+    double geld;
+    int mensen;
+    double uitkomst;
+
+    public void init() {
+        geld = 113;
+        mensen = 4;
+        uitkomst = geld / mensen;
+    }
+
+    public void paint(Graphics g){
+        setBackground(Color.orange);
+        g.setColor(Color.black);
+
+        g.drawString("Iedereen krijgt " + uitkomst + " euro.",20,20);
+
+
+    }
+}
