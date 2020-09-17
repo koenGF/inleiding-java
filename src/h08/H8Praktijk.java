8509b4a6f4148472920ad4dea891623d356a6abc
KoenGF <2144264@talnet.nl>
2020-09-17 18:55:40 +0200

    edited H8praktijk

--- src/h08/H8Praktijk.java
+++ src/h08/H8Praktijk.java
@@ -19,16 +19,16 @@ public class H8Praktijk extends Applet {
     int minusIs;
 
     public void init() {
-        tekstvak1 = new TextField("type hier iets", 30);
-        tekstvak2 = new TextField("type hier iets",30);
+        tekstvak1 = new TextField("getal 1", 10);
+        tekstvak2 = new TextField("getal 2",10);
         knopMultiply = new Button("*");
         knopMultiply.addActionListener( new KnopMultiplyListener() );
         knopDivide = new Button("/");
-        //knopDivide.addActionListener( new KnopDivideListener() );
-        knopPlus = new Button("*");
-        //knopPlus.addActionListener( new KnopPlusListener() );
-        knopMinus = new Button("*");
-        //knopMinus.addActionListener( new KnopMinusListener() );
+        knopDivide.addActionListener( new KnopDivideListener() );
+        knopPlus = new Button("+");
+        knopPlus.addActionListener( new KnopPlusListener() );
+        knopMinus = new Button("-");
+        knopMinus.addActionListener( new KnopMinusListener() );
         add(tekstvak1);
         add(tekstvak2);
         add(knopMultiply);
@@ -37,10 +37,46 @@ public class H8Praktijk extends Applet {
         add(knopMinus);
     }
 
+
+    //klaar als ik weet hoe een int in een tekstvak komt.
+
     class KnopMultiplyListener implements ActionListener	{
         public void actionPerformed( ActionEvent e ) {
             getal1 = Integer.parseInt(tekstvak1.getText());
             getal2 = Integer.parseInt(tekstvak2.getText());
+            multiplyIs = getal1 * getal2;
+            tekstvak1.setText("");
+            tekstvak2.setText("");
+            repaint();
+        }
+    }
+    class KnopDivideListener implements ActionListener	{
+        public void actionPerformed( ActionEvent e ) {
+            getal1 = Integer.parseInt(tekstvak1.getText());
+            getal2 = Integer.parseInt(tekstvak2.getText());
+            divideIs = getal1 / getal2;
+            tekstvak1.setText("");
+            tekstvak2.setText("");
+            repaint();
+        }
+    }
+    class KnopPlusListener implements ActionListener	{
+        public void actionPerformed( ActionEvent e ) {
+            getal1 = Integer.parseInt(tekstvak1.getText());
+            getal2 = Integer.parseInt(tekstvak2.getText());
+            plusIs = getal1 + getal2;
+            tekstvak1.setText("");
+            tekstvak2.setText("");
+            repaint();
+        }
+    }
+    class KnopMinusListener implements ActionListener	{
+        public void actionPerformed( ActionEvent e ) {
+            getal1 = Integer.parseInt(tekstvak1.getText());
+            getal2 = Integer.parseInt(tekstvak2.getText());
+            minusIs = getal1 - getal2;
+            tekstvak1.setText("");
+            tekstvak2.setText("");
             repaint();
         }
     }
