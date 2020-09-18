22a4c4c476bd05c40c4024f4c5adbf14ae90ae3f
KoenGF <2144264@talnet.nl>
2020-09-18 19:59:34 +0200

    added H10Opdr4 WIP

---
+++ src/h10/H10Opdr4.java
@@ -0,0 +1,92 @@
+//TODO rename tekstvaklistener naar maand en jaar listener
+package h10;
+
+import java.awt.*;
+import java.applet.*;
+import java.awt.event.*;
+
+public class H10Opdr4 extends Applet{
+    TextField tekstvakMaand, tekstvakJaar;
+    String maand, dagen, s;
+    int maandGetal;
+
+    public void init() {
+        tekstvakMaand = new TextField("month",5);
+        tekstvakMaand.addActionListener( new TekstvakListener() );
+        tekstvakJaar = new TextField("Jaar",5);
+        tekstvakJaar.addActionListener( new TekstvakListener() );
+        add(tekstvakMaand);
+        add(tekstvakJaar);
+        maand = "???";
+        dagen = "???";
+    }
+
+    public void paint (Graphics g){
+        g.drawString(maand + ", heeft " + dagen + " dagen.",50,50);
+    }
+    class TekstvakListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            s = tekstvakMaand.getText();
+            maandGetal = Integer.parseInt( s);
+            switch(maandGetal) {
+                case 1:
+                    maand = "Januari";
+                    dagen = "31";
+                    break;
+                case 2:
+                    maand = "Februari";
+
+
+
+
+
+                    //dagen = "28/29";
+                    break;
+                case 3:
+                    maand = "Maart";
+                    dagen = "31";
+                    break;
+                case 4:
+                    maand = "April";
+                    dagen = "30";
+                    break;
+                case 5:
+                    maand = "Mei";
+                    dagen = "31";
+                    break;
+                case 6:
+                    maand = "Juni";
+                    dagen = "30";
+                    break;
+                case 7:
+                    maand = "Juli";
+                    dagen = "31";
+                    break;
+                case 8:
+                    maand = "Augustus";
+                    dagen = "31";
+                    break;
+                case 9:
+                    maand = "September";
+                    dagen = "30";
+                    break;
+                case 10:
+                    maand = "Oktober";
+                    dagen = "31";
+                    break;
+                case 11:
+                    maand = "November";
+                    dagen = "30";
+                case 12:
+                    maand = "December";
+                    dagen = "31";
+                    break;
+                default:
+                    maand = "???";
+                    dagen= "???";
+                    break;
+            }
+            repaint();
+        }
+    }
+}
