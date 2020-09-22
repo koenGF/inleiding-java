53454e9a9eae3da674e54a3b9b2b70ecfa6ff66f
KoenGF <2144264@talnet.nl>
2020-09-22 15:45:58 +0200

    finished H10Opdr5

--- src/h10/H10Opdr5.java
+++ src/h10/H10Opdr5.java
@@ -7,29 +7,47 @@ import java.awt.event.*;
 public class H10Opdr5 extends Applet {
 TextField tekstvak;
 String tekst, g;
-Double getal;
+Double getal,totaal,gemiddelde;
+int aantalCijfers;
 
     public void init() {
         tekstvak = new TextField("",5);
         tekstvak.addActionListener( new tekstvakListener() );
         add(tekstvak);
         tekst = "";
+        totaal = 0.0;
+        aantalCijfers = 0;
+        gemiddelde = 0.0;
     }
 
     public void paint (Graphics g) {
         g.drawString(tekst,50,50);
+        g.drawString("het gemiddelde is: " + gemiddelde,50,70);
     }
     class tekstvakListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
+
+            //getal
             g = tekstvak.getText();
             getal = Double.parseDouble(g);
-            if ( getal > 5 ) {
+            System.out.println(getal);
+
+            //totaal
+            totaal += getal;
+
+            //aantalcijfers
+            aantalCijfers++;
+
+            //gemiddelde
+            gemiddelde = totaal / aantalCijfers;
+
+            if ( getal >= 5.5 ) {
                 tekst = "voldoende";
             }
             else {
                 tekst = "onvoldoende";
-                repaint();
             }
+            repaint();
         }
     }
 }
