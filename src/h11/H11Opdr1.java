f1f90849d6b6c60ffe6d33cf956ab9fa817e78a8
KoenGF <2144264@talnet.nl>
2020-09-25 13:34:28 +0200

    edited H11Opdr1

--- src/h11/H11Opdr1.java
+++ src/h11/H11Opdr1.java
@@ -8,13 +8,14 @@ public class H11Opdr1 extends Applet {
     public void init() {}
 
     public void paint(Graphics g) {
-        int teller;
+        int teller = 0;
         int x = 0;
 
-        for(teller = 1; teller <= 10; teller++) {
+        while(teller <= 10) {
             x += 20;
             g.drawLine(x , 50, x, 300 );
             g.drawString("" + teller,x,40);
+            teller++;
         }
     }
 }
---
+++ src/h11/H11Opdr2.java
@@ -0,0 +1,4 @@
+package h11;
+
+public class H11Opdr2 {
+}
