c08094f5d4bf738bb0d31d92c93fa5ef9b348011
KoenGF <2144264@talnet.nl>
2020-09-25 13:38:37 +0200

    finished H11Opdr2

--- src/h11/H11Opdr2.java
+++ src/h11/H11Opdr2.java
@@ -1,4 +1,21 @@
 package h11;
 
-public class H11Opdr2 {
+import java.awt.*;
+import java.applet.*;
+
+
+public class H11Opdr2 extends Applet {
+
+    public void init() {}
+
+    public void paint(Graphics g) {
+        int teller = 20;
+        int x = 0;
+
+        while(teller > 10) {
+            x += 20;
+            g.drawString("" + teller, x, 20 );
+            teller--;
+        }
+    }
 }
