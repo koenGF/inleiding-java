d8f62a490307cf634f88c884f791e2d35a91bdfd
KoenGF <2144264@talnet.nl>
2020-09-28 18:54:43 +0200

    finished H11Opdr9

--- src/h11/H11Opdr9.java
+++ src/h11/H11Opdr9.java
@@ -10,8 +10,8 @@ public class H11Opdr9 extends Applet {
 
     public void paint(Graphics g) {
         g.setColor(Color.black);
+        g.drawRect(20,20,160,160);
         int x = 40, y = 20, lengte = 20;
-
         while(true) {
             g.fillRect(x,y,lengte,lengte);
             y += 40;
@@ -19,6 +19,18 @@ public class H11Opdr9 extends Applet {
                 x += 40;
                 y = 20;
                 if(x >= 180) break;
+            }
+        }
+
+        x = 20;
+        y= 40;
+        while(true) {
+            g.fillRect(x,y,lengte,lengte);
+            y += 40;
+            if(y >= 180) {
+                x += 40;
+                y = 40;
+                if (x >= 180) break;
             }
         }
     }
