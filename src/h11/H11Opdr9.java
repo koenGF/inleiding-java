22d00be8400323c7cfe4d64d9f48b64e1378eb52
KoenGF <2144264@talnet.nl>
2020-09-28 18:00:33 +0200

    added H11Opdr9 WIP

---
+++ src/h11/H11Opdr9.java
@@ -0,0 +1,25 @@
+package h11;
+
+import java.awt.*;
+import java.applet.*;
+
+
+public class H11Opdr9 extends Applet {
+
+    public void init() {}
+
+    public void paint(Graphics g) {
+        g.setColor(Color.black);
+        int x = 40, y = 20, lengte = 20;
+
+        while(true) {
+            g.fillRect(x,y,lengte,lengte);
+            y += 40;
+            if(y >= 180) {
+                x += 40;
+                y = 20;
+                if(x >= 180) break;
+            }
+        }
+    }
+}
