b0ec07e8cf5927ea0c5a9a75b6a6b367f97e68db
KoenGF <2144264@talnet.nl>
2020-09-28 16:31:19 +0200

    edited H11Opdr3 & finished H11Opdr5

--- src/h11/H11Opdr3.java
+++ src/h11/H11Opdr3.java
@@ -9,15 +9,15 @@ public class H11Opdr3 extends Applet {
     public void init() {}
 
     public void paint(Graphics g) {
-        int prev2Getal = 1, prevGetal = 1, currentGetal, x = 60;
+        int prev2Getal = 1, prevGetal = 1, currentGetal, x = 80;
 
         g.drawString("" + prev2Getal,20,20);
-        g.drawString("" + prevGetal,40,20);
+        g.drawString("" + prevGetal,50,20);
         while(true) {
             currentGetal = prevGetal + prev2Getal;
-            if (currentGetal > 200) break;
+            if (currentGetal > 1000) break;
             g.drawString("" + currentGetal, x, 20 );
-            x += 20;
+            x += 30;
             prev2Getal = prevGetal;
             prevGetal = currentGetal;
         }
---
+++ src/h11/H11Opdr5.java
@@ -0,0 +1,34 @@
+package h11;
+
+import java.awt.*;
+import java.applet.*;
+
+
+public class H11Opdr5 extends Applet {
+
+    public void init() {}
+
+    public void paint(Graphics g) {
+        int vertX = 20,vertY1 = 10,vertY2 = 30,horiX1 = 10,horiX2 = 30,horiY = 20;
+
+        //linksboven
+        g.drawLine(10,10,20,10);
+        g.drawLine(10,10,10,20);
+
+        //rechtsonder
+        g.drawLine(60,50,60,60);
+        g.drawLine(50,60,60,60);
+
+        while(vertY2 <= 60 && horiX2 <= 60) {
+            g.drawLine(vertX,vertY1,vertX,vertY2);
+            vertX += 10;
+            vertY1 += 10;
+            vertY2 += 10;
+
+            g.drawLine(horiX1,horiY,horiX2,horiY);
+            horiX1 += 10;
+            horiX2 += 10;
+            horiY += 10;
+        }
+    }
+}
