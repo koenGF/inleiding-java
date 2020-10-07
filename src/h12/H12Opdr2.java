8cf46d812472f88d46c564db116d9cedbdae8625
KoenGF <2144264@talnet.nl>
2020-10-07 10:41:09 +0200

    finished H12Opdr2

--- src/h12/H12Opdr2.java
+++ src/h12/H12Opdr2.java
@@ -2,19 +2,16 @@ package h12;
 
 import java.awt.*;
 import java.applet.*;
-import java.util.*;
 
 public class H12Opdr2 extends Applet {
 int teller =0;
-Button[] knoppen = {};
+Button[] knoppen;
 
     public void init() {
         knoppen = new Button [25];
-        add(knoppen[teller]);
-    }
 
-    public void paint(Graphics g) {
         while(teller < knoppen.length) {
+            knoppen[teller] = new Button("button" + teller);
             add(knoppen[teller]);
             teller++;
         }
