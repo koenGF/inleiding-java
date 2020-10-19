2dc59dd7433133f30c9415d196b421eb6e25fe70
KoenGF <2144264@talnet.nl>
2020-10-19 14:53:03 +0200

    added H13Opdr1 WIP

---
+++ src/h13/H13Opdr1.java
@@ -0,0 +1,20 @@
+package h13;
+/*
+maak een methode die een driekhoek tekent. gebruikt drie punten, verbind die met lijnen.
+ */
+import java.awt.*;
+import java.applet.*;
+
+public class H13Opdr1 extends Applet {
+
+    public void init() {
+        tekenDriehoek(null,0,100,100,100,50,0);
+            }
+
+    void tekenDriehoek( Graphics g, int x1, int y1, int x2, int y2, int x3, int y3) {
+        // teken een driehoek m.b.v. de meegegeven data (parameters)
+        g.drawLine(x1,y1,x2,y2);
+        g.drawLine(x2,y2,x3,y3);
+        g.drawLine(x3,y3,x1,y1);
+    }
+}
