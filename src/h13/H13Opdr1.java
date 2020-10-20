48c905161a66f85f330f057c4c4536295cb3edaf
KoenGF <2144264@talnet.nl>
2020-10-20 12:42:17 +0200

    finished H13Opdr1

--- src/h13/H13Opdr1.java
+++ src/h13/H13Opdr1.java
@@ -8,10 +8,13 @@ import java.applet.*;
 public class H13Opdr1 extends Applet {
 
     public void init() {
-        tekenDriehoek(null,0,100,100,100,50,0);
-            }
+    }
 
-    void tekenDriehoek( Graphics g, int x1, int y1, int x2, int y2, int x3, int y3) {
+    public void paint(Graphics g) {
+            tekenDriehoek(g,0,100,100,100,50,0);
+    }
+
+    public void tekenDriehoek( Graphics g, int x1, int y1, int x2, int y2, int x3, int y3) {
         // teken een driehoek m.b.v. de meegegeven data (parameters)
         g.drawLine(x1,y1,x2,y2);
         g.drawLine(x2,y2,x3,y3);
