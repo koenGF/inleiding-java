c0cf453c76003537c0c41508d02a99463a209222
KoenGF <2144264@talnet.nl>
2020-10-22 14:02:30 +0200

    added H14Opdr2 WIP

---
+++ src/h14/H14Opdr2.java
@@ -0,0 +1,20 @@
+package h14;
+/*
+verdeel een deck van 52 kaarten (geen jokers) over 4 spelers.
+speel applaus.wav als het klaar is.
+ */
+import java.awt.*;
+import java.applet.*;
+import java.util.*;
+import java.awt.event.*;
+
+public class H14Opdr2 extends Applet {
+
+    public void init() {
+
+    }
+
+    public void paint(Graphics g) {
+
+    }
+}
